102261fd50d45ba65ddaf53e5b49acda6dfe2229
Michael Wolber <mwolber@gmx.de>
2025-10-23 01:30:45 +0200

    Added mutex for player-cache.

--- elo/events/events_test/matchend_db_test.go
+++ elo/events/events_test/matchend_db_test.go
@@ -9,14 +9,14 @@ func TestMatchEndsByDB(t *testing.T) {
 	var dbmatchescount int
 	matchescount := countMatchEnds()
 
-	row := store.Db.QueryRow(context.Background(), "select count(matches.id) from matches ")
+	row := store.Db.QueryRow(context.Background(), "select count(matches.id) from matches where completed ")
 	err := row.Scan(&dbmatchescount)
 	if err != nil {
 		panic(err.Error())
 	}
 
 	//if matchescount != dbmatchescount { // can't be equal, because we cannot correctly count started, but not finished maps using grep only
-	if matchescount != matchescount {
+	if matchescount != dbmatchescount {
 		t.Errorf("%s failed: filecount %d != dbcount %d", t.Name(), matchescount, dbmatchescount)
 	}
 }
